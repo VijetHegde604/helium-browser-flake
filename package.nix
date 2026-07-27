2826a2c9a0cc0b014cdce7591fc658c83c559a48
Vijet Hegde <120740615+VijetHegde604@users.noreply.github.com>
2026-07-27 18:08:03 +0530

    Improve desktop integration

--- package.nix
+++ package.nix
@@ -4,9 +4,6 @@
   fetchurl,
   makeWrapper,
   autoPatchelfHook,
-  copyDesktopItems,
-  makeDesktopItem,
-
   alsa-lib,
   atk,
   cairo,
@@ -44,7 +41,6 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [
     autoPatchelfHook
     makeWrapper
-    copyDesktopItems
   ];
 
   buildInputs = [
@@ -96,30 +92,72 @@ stdenv.mkDerivation rec {
       $out/bin/helium \
       --add-flags "--ozone-platform-hint=auto"
 
-    mkdir -p $out/share/icons/hicolor/256x256/apps
-    cp $out/opt/helium/product_logo_256.png \
-      $out/share/icons/hicolor/256x256/apps/helium.png
+    for icon in $out/opt/helium/product_logo_*.png; do
+      [ -e "$icon" ] || continue
+      size="''${icon##*_}"
+      size="''${size%.png}"
+      case "$size" in
+        [0-9]* ) ;;
+        * ) continue ;;
+      esac
+
+      install -Dm644 "$icon" \
+        "$out/share/icons/hicolor/''${size}x''${size}/apps/helium.png"
+    done
+
+    mkdir -p $out/share/applications
+    upstreamDesktop="$(find $out/opt/helium -name '*.desktop' -print -quit)"
+    if [ -n "$upstreamDesktop" ]; then
+      install -Dm644 "$upstreamDesktop" $out/share/applications/helium.desktop
+      sed -i -E \
+        -e '0,/^Exec=/{s|^Exec=.*|Exec=helium %U|}' \
+        -e '0,/^Icon=/{s|^Icon=.*|Icon=helium|}' \
+        $out/share/applications/helium.desktop
+      if ! grep -q '^StartupWMClass=' $out/share/applications/helium.desktop; then
+        awk '
+          /^\[Desktop Action / && ! inserted {
+            print "StartupWMClass=Helium"
+            inserted = 1
+          }
+          { print }
+          END {
+            if (! inserted) {
+              print "StartupWMClass=Helium"
+            }
+          }
+        ' $out/share/applications/helium.desktop > $out/share/applications/helium.desktop.tmp
+        mv $out/share/applications/helium.desktop.tmp $out/share/applications/helium.desktop
+      fi
+    else
+      cat > $out/share/applications/helium.desktop <<EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Helium
+GenericName=Web Browser
+Comment=Access the Internet
+Exec=helium %U
+Terminal=false
+Icon=helium
+Categories=Network;WebBrowser;
+MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;image/webp;x-scheme-handler/http;x-scheme-handler/https;
+StartupNotify=true
+StartupWMClass=Helium
+Keywords=browser;internet;web;www;
+Actions=new-window;new-private-window;
+
+[Desktop Action new-window]
+Name=New Window
+Exec=helium --new-window %U
+
+[Desktop Action new-private-window]
+Name=New Private Window
+Exec=helium --incognito %U
+EOF
+    fi
 
     runHook postInstall
   '';
-
-  desktopItems = [
-    (makeDesktopItem {
-      name = "helium";
-      desktopName = "Helium";
-      exec = "helium %U";
-      icon = "helium";
-      categories = [
-        "Network"
-        "WebBrowser"
-      ];
-      mimeTypes = [
-        "x-scheme-handler/http"
-        "x-scheme-handler/https"
-        "text/html"
-      ];
-    })
-  ];
 
   meta = {
     description = "Privacy-focused browser based on ungoogled Chromium";
